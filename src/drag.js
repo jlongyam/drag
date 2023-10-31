43c5bed6352992614971edac5b39a005873bcbc9
jlongyam <jhonliongbox@gmail.com>
2023-10-31 12:03:07 +0700

    Add sample drag sort

--- src/drag.js
+++ src/drag.js
@@ -1,4 +1,4 @@
-function draggAble(o_scope, o_drag, cb = {}) {
+function drag(o_scope, o_drag, cb = {}) {
   o_scope.addEventListener('dragover', (e) => {
     e.preventDefault()
     if (cb.dragover) cb.dragover(e)
@@ -14,82 +14,79 @@ function draggAble(o_scope, o_drag, cb = {}) {
     if (cb.drop) cb.drop(e)
   })
   o_drag.addEventListener('drag', (e) => {
+    o_drag.style.visibility = 'hidden'
+    o_drag.style.left = e.clientX + 'px'
+    o_drag.style.top = e.clientY + 'px'
     if (cb.drag) cb.drag(e)
   })
   o_drag.addEventListener('dragstart', (e) => {
     if (cb.dragstart) cb.dragstart(e)
   })
   o_drag.addEventListener('dragend', (e) => {
+    o_drag.style.visibility = 'unset'
     if (cb.dragend) cb.dragend(e)
   })
 }
-function dragMouse(o_scope, o_drag, cb = {}) {
+function dragMove(o_scope, o_drag, cb = {}) {
   let
     posX = 0,
     posY = 0,
     pointX = 0,
     pointY = 0,
-    mouseX = 0,
-    mouseY = 0,
     touch = false
     ;
-  const drag = (e) => {
+  function dragging(e) {
     if (touch) {
       pointX = touch.clientX - o_scope.offsetLeft
       pointY = touch.clientY - o_scope.offsetTop
     }
     else {
-
       pointX = e.clientX - posX;
       pointY = e.clientY - posY;
     }
-    // console.log(e.type)
-    max = {
+    let max = {
       left: o_scope.offsetLeft,
       top: o_scope.offsetTop,
       right: o_scope.offsetLeft + o_scope.offsetWidth - o_drag.offsetWidth,
       bottom: o_scope.offsetTop + o_scope.offsetHeight - o_drag.offsetHeight
     }
-
-    if (pointX <= max.left) {
-      pointX = max.left
-    }
-    else if (pointX >= max.right) {
-      pointX = max.right
-    } else {
-      if(touch) pointX = touch.clientX - o_scope.offsetLeft
+    if (pointX <= max.left) pointX = max.left
+    else if (pointX >= max.right) pointX = max.right
+    else {
+      if (touch) pointX = touch.clientX - o_scope.offsetLeft
       else pointX = e.clientX - posX;
     }
-    if (pointY <= max.top) {
-      pointY = max.top
-    } else if (pointY >= max.bottom) {
-      pointY = max.bottom
-    } else {
-      if (touch ) pointY = touch.clientY - o_scope.offsetTop
+    if (pointY <= max.top) pointY = max.top
+    else if (pointY >= max.bottom) pointY = max.bottom
+    else {
+      if (touch) pointY = touch.clientY - o_scope.offsetTop
       else pointY = e.clientY - posY;
     }
     o_drag.style.left = pointX + 'px'
     o_drag.style.top = pointY + 'px'
-    if (cb.drag) cb.drag(e)
+    if (cb.drag) cb.dragging(e)
   }
-  const mouseDown = (e) => {
+  function mouseDown(e) {
     e.preventDefault();
     posX = e.clientX - o_drag.offsetLeft
     posY = e.clientY - o_drag.offsetTop
     touch = false
-    o_scope.addEventListener('mousemove', drag, false)
+    o_scope.addEventListener('mousemove', dragging, false)
     if (cb.mouseDown) cb.mouseDown(e)
   }
-  const touchMove = (e) => {
+  function mouseUp(e) {
+    o_scope.removeEventListener('mousemove', dragging, false)
+    if (cb.mouseUp) cb.mouseUp(e)
+  }
+  function touchMove(e) {
     e.preventDefault();
     if (e.type === 'touchmove') touch = e.targetTouches[0]
-    o_scope.addEventListener('touchmove', drag, false)
+    o_scope.addEventListener('touchmove', dragging, false)
+    if (cb.touchmove) cb.touchmove(e)
   }
-  function mouseUp() {
-    o_scope.removeEventListener('mousemove', drag, false)
-  }
-  function touchEnd() {
-    o_scope.removeEventListener('touchmove', drag, false)
+  function touchEnd(e) {
+    o_scope.removeEventListener('touchmove', dragging, false)
+    if (cb.touchEnd) cb.touchEnd(e)
   }
   o_drag.addEventListener('mousedown', mouseDown, false)
   o_scope.addEventListener('mouseup', mouseUp, false)
